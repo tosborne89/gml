426aa1525505f06eda3abfc3971646080d0e9333
Tim Osborne <osborne.tim89@gmail.com>
2017-09-24 17:37:25 +0200

    gml initial so changes are visible

---
+++ skin/frontend/gml/default/scss/config.rb
@@ -0,0 +1,11 @@
+# note: this should never truly be refernced since we are using relative assets
+http_path = "/skin/frontend/rwd/default/"
+css_dir = "../css"
+sass_dir = "../scss"
+images_dir = "../images"
+javascripts_dir = "../js"
+relative_assets = true
+
+line_comments = true
+output_style = :expanded
+environment = :development
